3bf900622276088100efd9039b78b0b2a439bd93
saumya-singh <saumya.singh0993@gmail.com>
2018-05-18 02:15:27 +0530

    add feature to select player1 and player2

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room, rooms
-import json
+import json, random
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -17,18 +17,32 @@ def grid():
     return render_template('grid.html')
 
 
-@socketio.on('join_player')
-def value_changed(player_id):
+@socketio.on('join player')
+def join_player(player_id):
     player = json.loads(player_id)
     username = player['username']
     room = player['id']
     join_room(room)
     if room not in players_connected:
-        players_connected[room] = [request.sid]
+        players_connected[room] = {username: request.sid}
         emit('user update', username + ' has entered the room.', room=room)
     elif len(players_connected) == 1:
-        players_connected[room].append(request.sid)
-        emit('users joined', [username + ' has entered the room.', room, players_connected[room]], room=room)
+        players_connected[room].update({username: request.sid})
+        emit('users joined', [username + ' has joined the room.', room, players_connected[room]], room=room)
+
+
+@socketio.on('select player')
+def select_player(room_details):
+    print("=================================", room_details)
+    people = list(room_details[1].keys())
+    print("==========people: ", people)
+    player1 = random.choice(people)
+    print("==========people111: ", type(player1))
+    people.remove(player1)
+    player2 = people.pop()
+    print("==========people222: ", player2)
+    player_dict = {"player1": player1, "player2": player2}
+    emit('display', [player_dict, room_details[1], room_details], room=room_details[1])
 
 
 if __name__ == '__main__':
